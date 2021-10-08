7b907e748bd54604f1948ca9c6748ff5e0fe5ed2
Thai Le <thai.viet.le@gmail.com>
2021-10-08 16:45:33 -0500

    build

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,14 +1,14 @@
 var plugins = [{
       name: 'gatsby-plugin-react-helmet',
-      plugin: require('/Users/thaile/dev/thai-portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/thaile/dev/buddharage.github.io/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-image',
-      plugin: require('/Users/thaile/dev/thai-portfolio/node_modules/gatsby-plugin-image/gatsby-ssr'),
+      plugin: require('/Users/thaile/dev/buddharage.github.io/node_modules/gatsby-plugin-image/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-manifest',
-      plugin: require('/Users/thaile/dev/thai-portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/thaile/dev/buddharage.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","display":"minimal-ui","icon":"src/images/favicon-32x32.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"51a307fc2af1ef294bb3c16fd89c798f"},
     }]
 /* global plugins */
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,8 +4,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---src-pages-404-js": preferDefault(require("/Users/thaile/dev/thai-portfolio/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/thaile/dev/thai-portfolio/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/Users/thaile/dev/thai-portfolio/src/pages/page-2.js"))
+  "component---src-pages-404-js": preferDefault(require("/Users/thaile/dev/buddharage.github.io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/thaile/dev/buddharage.github.io/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/thaile/dev/buddharage.github.io/src/pages/page-2.js"))
 }
 
